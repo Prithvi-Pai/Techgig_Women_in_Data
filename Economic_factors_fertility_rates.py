8b7a0c6811da4ed2ae8aac0e2c97596bbfdb8c02
Prithvi-Pai <paiprithvi93@gmail.com>
2024-01-04 12:17:35 +0530

    Update Economic_factors_fertility_rates.py
    Change in caps

--- Economic_factors_fertility_rates.py
+++ Economic_factors_fertility_rates.py
@@ -1,4 +1,4 @@
 import streamlit
 
 streamlit.title('TechGig Women in Data')
-Streamlit.header('Hypothesis on Economic Empowerment and Marital Status')
+streamlit.header('Hypothesis on Economic Empowerment and Marital Status')
