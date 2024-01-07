36a8a874e7ab8afd668ac6653288ece43a0935d4
Prithvi-Pai <paiprithvi93@gmail.com>
2024-01-07 14:09:13 +0530

    Update Economic_factors_fertility_rates.py
    commented code data. 
    added new code for data

--- Economic_factors_fertility_rates.py
+++ Economic_factors_fertility_rates.py
@@ -17,7 +17,8 @@ my_cur.execute('select "States/UTs" ,AREA , "Women (age 15-49)  with 10 or more 
 
 result = my_cur.fetchall()
 
-data = streamlit.dataframe(result, columns=['"States/UTs"', 'AREA', '"Women (age 15-49)  with 10 or more years of schooling (%)"','"Total Fertility Rate (number of children per woman)"'])
+# data = streamlit.dataframe(result, columns=['"States/UTs"', 'AREA', '"Women (age 15-49)  with 10 or more years of schooling (%)"','"Total Fertility Rate (number of children per woman)"'])
+data = streamlit.dataframe(result)
 
 # Streamlit app
 streamlit.title('Inverse Relationship between Education and Fertility')
